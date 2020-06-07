756463800ae41160f39a880b7a521343e5474d6c
smirza3 <saf.mirza@live.co.uk>
2020-06-07 16:25:17 +0100

    Updated answers for 4

--- session_10/answers/A4.py
+++ session_10/answers/A4.py
@@ -1,5 +1,33 @@
 
-# 4. Create a Employee class and initialise it with name and staff number. Make methods to:
-#  - display_info - It should display all the information of the employee.
-#  - set_department - It should assign the department to employee.
-#  - set_bonus - It should assign a bonus amount to the employee.
+# 3. Create a Vehicle parent class, initialise it with, wheels, colour and a method to display all this information.
+#     - i. Create a Tesla (or any car brand) child classs and add a method to get the miles and a method to display all this information.
+#     - ii. Change the colour of the vehicle.
+#     - iii. Delete the wheels.
+
+class Vehicle:
+    def __init__(self, wheels, colour):
+        self.wheels = wheels
+        self.colour = colour 
+    
+    def display_info(self):
+        print("This car is " + self.colour + " and has " + str(self.wheels) + " wheels.")
+
+class Tesla(Vehicle):
+    def __init__(self, wheels, colour, miles):
+        super().__init__(wheels, colour)
+        self.miles = miles 
+    
+    def display_more_info(self):
+        print("This car is " + self.colour + " and has " + str(self.wheels) + " wheels and " + str(self.miles) + " miles.")
+
+tesla = Tesla(4, "black", 20000)
+tesla.display_more_info()
+
+tesla.colour = "red"
+tesla.display_more_info()
+
+del tesla.wheels
+tesla.display_more_info()
+
+
+
