173509988c5fc34f5a2e67738558b51b081eaf3f
smirza3 <saf.mirza@live.co.uk>
2020-06-07 16:37:22 +0100

    Updated question 5

--- session_10/answers/A5.py
+++ session_10/answers/A5.py
@@ -1,3 +1,17 @@
-# 5. Write a class which has two methods get_word and print_word. 
-#  - get_word accepts a string from the user and print_word prints the string in upper case. 
+# 5. Create a class called Upper which has two methods called get_word and print_word. 
+#  - the get_word method should accept a string from the user
+#  - the print_word method prints the string in upper case. 
 
+class Upper:
+    def __init__(self):
+        self.word = ""
+    
+    def get_word(self):
+        self.word = input("Input a word:\n")
+    
+    def print_word(self):
+        print(self.word.upper())
+
+word1 = Upper()
+word1.get_word()
+word1.print_word()
