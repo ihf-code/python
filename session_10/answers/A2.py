e0e85094caf562d303f733f8aa6f9471e6314f3f
smirza3 <saf.mirza@live.co.uk>
2020-06-07 15:01:08 +0100

    Updated answer for question 2

--- session_10/answers/A2.py
+++ session_10/answers/A2.py
@@ -1,3 +1,21 @@
-# 2. Create a Circle class, initialise it with a radius. The circles constructed must have two getters get_area() and get_perimeter() which give both respective areas and perimeter (circumference).
-#  - Note: Area of a circle = π r ** 2 
-#  - Circumference = π × Diameter
+# 2. Create a Circle class, initialise it with a radius. Create two methods for the Circle class: get_area() and get_circumference() which give both respective areas and circumference.
+#  - Note: Area of a circle = πr ** 2 
+#  - Circumference = 2πr
+
+class Circle:
+    pi = 3.14159265
+    
+    def __init__(self, radius):
+        self.radius = radius
+
+    def get_area(self):
+        print(self.pi * (self.radius ** 2))
+
+    def get_circumference(self):
+        print(self.pi * (self.radius * 2))
+
+circy = Circle(11)
+circy.get_area()
+
+circy = Circle(4.44)
+circy.get_circumference()
