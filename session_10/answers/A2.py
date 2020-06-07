4973da88bdcad23c398695a89cec08f16c726da5
smirza3 <saf.mirza@live.co.uk>
2020-06-07 15:07:45 +0100

    Updated answer for question 2

--- session_10/answers/A2.py
+++ session_10/answers/A2.py
@@ -1,6 +1,7 @@
-# 2. Create a Circle class, initialise it with a radius. Create two methods for the Circle class: get_area() and get_circumference() which give both respective areas and circumference.
-#  - Note: Area of a circle = πr ** 2 
-#  - Circumference = 2πr
+# 2. Create a Circle class, initialise it with a radius. Create two methods for the Circle class: get_area() and get_circumference() which give both respective areas and circumference, to 2 decimal placese.
+# - Note: Area of a circle = πr ** 2 
+# - Circumference = 2πr
+# - Use the round() function to get the answer to 2 decimal places 
 
 
 class Circle:
