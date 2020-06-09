9da8a85cb53481d759bc78cb3c19b89d1eb17071
smirza3 <saf.mirza@live.co.uk>
2020-06-09 17:53:03 +0100

    added comments to answers for week 3

--- session_03/answers/A2.py
+++ session_03/answers/A2.py
@@ -1,6 +1,10 @@
 # A2 - Ask for the user's name, if they are not called "Alice", print "You're not Alice!"
 
+# Created variable name and assigned the input to it. 
+# Whatever the user enters as their name will be stored in the name variable.
 name = input("What is your name?\n")
 
+# Checking if the name the user has input is not Alice. 
+# If it is not Alice, print "You're not Alice!"
 if name != "Alice":
     print("You're not Alice!")
