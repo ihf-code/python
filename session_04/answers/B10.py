4da0c0bc8d54c9a7df07822746afdecc130a90ee
smirza3 <saf.mirza@live.co.uk>
2020-06-09 10:34:30 +0100

    Updated question numbere

--- session_04/answers/B10.py
+++ session_04/answers/B10.py
@@ -1,4 +1,4 @@
-# D5 - FizzBuzz – Write a program that prints the numbers from 1 to 100.
+# B10 - FizzBuzz – Write a program that prints the numbers from 1 to 100.
 # For multiples of three, print “Fizz” instead of the number and for multiples of five, print “Buzz”.
 # For numbers which are multiples of both three and five, print “FizzBuzz”
 
